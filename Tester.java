3698e4a0f22cd710f42c6c1a69a457afc6a20ad0
Kenli8504 <kli22@stuy.edu>
2020-10-24 17:25:04 -0400

    Tested the Largest Method

--- Tester.java
+++ Tester.java
@@ -2,5 +2,6 @@ public class Tester{
   public static void main(String [] args) {
     int [] a = {3,4,1,2,5,6};
     System.out.println(ArrayOps.sum(a));
+    System.out.println(ArrayOps.largest(a));
   }
 }
