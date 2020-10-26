854fae13b9989e7d4112832cca8cc755d3c51861
Kenli8504 <kli22@stuy.edu>
2020-10-26 17:51:52 -0400

    Created the 2D array, tested with java.util.arrays

--- Tester.java
+++ Tester.java
@@ -1,7 +1,12 @@
+import java.util.Arrays;
 public class Tester{
   public static void main(String [] args) {
     int [] a = {3,4,1,2,5,6};
+    int [][] b = {{1,2,3,4,5},
+                {3,2,1,6,4},
+                {3,5,21,6,-2}};
     System.out.println(ArrayOps.sum(a));
     System.out.println(ArrayOps.largest(a));
+    System.out.println(Arrays.toString(ArrayOps.sumRows(b)));
   }
 }
